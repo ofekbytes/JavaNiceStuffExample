39b14992bd4894ec41d002b0e10912b2a73d1171
Kessler Yaron <yaron.kessler42@gmail.com>
2017-07-09 10:15:46 +0300

    ex

---
+++ src/z_testZone/Address.java
@@ -0,0 +1,8 @@
+package z_testZone;
+
+public interface Address {
+	
+	String Zip2Address (int Number);
+	int Address2Zip (int String); 
+
+}
---
+++ src/z_testZone/PesonalDetails.java
@@ -0,0 +1,10 @@
+package z_testZone;
+
+public interface PesonalDetails {
+	
+  void FirstName(String firstName);
+  void LastName(String lastName);
+  void Age (int age);
+  
+
+}
