bf71e85dd94bb8dc2b5131dd8a7a4513f93672cd
Kessler Yaron <yaron.kessler42@gmail.com>
2017-07-10 13:46:47 +0300

    ex

--- src/a_BasicEexample/A_javaBasicVariable.java
+++ src/a_BasicEexample/A_javaBasicVariable.java
@@ -134,16 +134,16 @@ public class A_javaBasicVariable {
 		System.out.println("the time is:  " + hh + ":" + mm + ":" + ss +  ":" + ms);
 		
 		
-		System.out.println("\nVariable max value:");
+		System.out.println("\nVariable / Atributes:");
 		
-		System.out.println("Byte == " + Byte.MIN_VALUE + " <--> " + Byte.MAX_VALUE); 
-		System.out.println("Short == " + Short.MIN_VALUE + " <--> " + Short.MAX_VALUE);
-		System.out.println("int / Integer == " + Integer.MIN_VALUE + " <--> " + Integer.MAX_VALUE);
-		System.out.println("Long == " + Long.MIN_VALUE + " <--> " + Long.MAX_VALUE);	
-		System.out.println("char / Character == " + Character.MIN_VALUE + " <--> " + Character.MAX_VALUE);
+		System.out.println("Byte (8 bit / 1 byte) == " + Byte.MIN_VALUE + " <--> " + Byte.MAX_VALUE);  
+		System.out.println("Short (16 bit / 2 byte) == " + Short.MIN_VALUE + " <--> " + Short.MAX_VALUE); 
+		System.out.println("int / Integer (32 bit / 4 byte) == " + Integer.MIN_VALUE + " <--> " + Integer.MAX_VALUE);
+		System.out.println("Long (64 bit / 8 byte)  == " + Long.MIN_VALUE + " <--> " + Long.MAX_VALUE);	
+		System.out.println("char / Character (16 bit / 2 byte)  == " + Character.MIN_VALUE + " <--> " + Character.MAX_VALUE);
 		
-        System.out.println("Float == " + Float.MIN_VALUE + " <--> " + Float.MAX_VALUE);
-        System.out.println("Double == " + Double.MIN_VALUE + " <--> " + Double.MAX_VALUE);
+        System.out.println("Float (32 bit / 4 byte) == " + Float.MIN_VALUE + " <--> " + Float.MAX_VALUE);
+        System.out.println("Double (64 bit / 8 byte) == " + Double.MIN_VALUE + " <--> " + Double.MAX_VALUE);
 	
         //static variable (no need for instance of the class.
         intCount++;
