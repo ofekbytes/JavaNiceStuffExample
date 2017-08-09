b0403bfbd17bbe8f75a7b447b9c3426558a47ce7
Kessler Yaron <yaron.kessler42@gmail.com>
2017-08-09 10:20:50 +0300

    test

--- src/z_example_basicExamples/cardWar.java
+++ src/z_example_basicExamples/cardWar.java
@@ -9,7 +9,25 @@ public class cardWar {
 	private static final int a = 13; //ace
 	//private static final int J = 14; //Jocker
 
+	
+	public int fncharToInt(char character)
+	{
+		
+		return 1;
+	}
 	 
+	
+	public int fnCheckWin(char chPlayer1, char cdPlayer2) {
+		
+		int intPlayer1=0;
+		int intPlayer2=0;
+		
+		intPlayer1 = Integer.parseUnsignedInt(chPlayer1);
+				
+		return 0;
+	}
+	
+	
 	public int solution(String cardsOfPlayerA, String cardsOfPlayerB) {
 		int win = 0;
 		char [] chA;
@@ -35,8 +53,20 @@ public class cardWar {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	
+
+		int number = 9;
+		
+		System.out.printf("j=%d, q=%d, k=%d, a=%d", j,q,k,a);
+		
+
+	    if (number > j)
+	    	System.out.printf("\nnumber=%d bigger then j=%d", number, j);
+	    
+	    if (j > number)
+	    	System.out.printf("\nnumber=%d smaller then j=%d", number, j);
+	    	
+	    System.out.println("\n\nthe end");
+	    
 	}
 
 }
--- src/z_example_mvc_contact/MVCContact.java
+++ src/z_example_mvc_contact/MVCContact.java
@@ -3,24 +3,15 @@
  */
 package z_example_mvc_contact;
 
-/**
- * @author yaron
- *
- */
+
 public class MVCContact {
 
-	/**
-	 * 
-	 */
 	public MVCContact() {
 		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
 
