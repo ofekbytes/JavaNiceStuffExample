f9e350b808712af2a76f9767f026a41267d29ffb
yaron <yaron.kessler41@gmail.com>
2017-09-11 13:10:51 +0300

    oop

--- src/d_ClassAndObjects/A_OOP.java
+++ src/d_ClassAndObjects/A_OOP.java
@@ -9,8 +9,20 @@ public class A_OOP {
 	
 	private String stName = "" ;
 	
+	//method without parameters
 	static void fnHello() {
 		System.out.println("Hello...");
+	}
+	
+	//method with parameter
+	static void fnHello(String NameParameters) 
+	{
+		System.out.println(NameParameters); 
+	}
+	
+	double sales_tax(double price)
+	{
+	    return 0.10 * price;
 	}
 	
 	/**
@@ -20,6 +32,9 @@ public class A_OOP {
 		
 	}
 	
+	/** 
+	 *  main()
+	 */
 	public static void main(String [] args) {
 		
 		//no need to write like that because fnHello() is a "static".
@@ -27,7 +42,10 @@ public class A_OOP {
 		ap.fnHello();
 		
 		fnHello(); //method/function name with parentheses.
+		fnHello("argument");
 		
+		double price = 7000;
+		System.out.println("price = " + price + " ,Tax == " + ap.sales_tax(price));
 	}
 
 }
