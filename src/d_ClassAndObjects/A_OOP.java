2ed6257998e524ae4836dcdea43d57fba9401e3d
yaron <yaron.kessler41@gmail.com>
2017-09-12 13:56:41 +0300

    java example

--- src/d_ClassAndObjects/A_OOP.java
+++ src/d_ClassAndObjects/A_OOP.java
@@ -5,22 +5,44 @@ package d_ClassAndObjects;
  * Object Oriented Programming (OOP) - 
  *
  */
+
+/***
+ * class declaration
+ */
 public class A_OOP {
 	
-	private String stName = "" ;
+	// Global variable / attributes
+	private String stFName = "";
+	private String stLName = "";
 	
-	//method without parameters
+
+	/****
+	 * method no parameters
+	 * return: void
+	 */
 	static void fnHello() {
 		System.out.println("Hello...");
 	}
 	
-	//method with parameter
+	
+	/****
+	 * method with one parameter
+	 * @param: NameParameters
+	 * return: void
+	 */
 	static void fnHello(String NameParameters) 
 	{
 		System.out.println(NameParameters); 
 	}
 	
-	double sales_tax(double price)
+	/****
+	 * 
+	 * method return a value (double type)
+	 * @param: price
+	 * @return: total tax price
+	 */
+
+	private double sales_tax(double price)
 	{
 	    return 0.10 * price;
 	}
@@ -37,15 +59,28 @@ public class A_OOP {
 	 */
 	public static void main(String [] args) {
 		
-		//no need to write like that because fnHello() is a "static".
+		
+		//declare an object "ap" of the class "A_OOP()"
 		A_OOP ap = new A_OOP();
+		A_OOP ap2 = new A_OOP();
+		
+		//"ap" is an object of type "A_OOP" (class) with a call to "fnHello()" method
 		ap.fnHello();
 		
+		//you can write like that because fnHello() is a "static".
 		fnHello(); //method/function name with parentheses.
 		fnHello("argument");
 		
 		double price = 7000;
 		System.out.println("price = " + price + " ,Tax == " + ap.sales_tax(price));
+		
+		//writing to object attributes
+		ap.stFName = "ap === tea";
+		ap2.stFName = "ap2 === coffie";
+	
+		//reading from object attributes
+		System.out.println("object " + ap.stFName);
+		System.out.println("object " + ap2.stFName);
 	}
 
 }
