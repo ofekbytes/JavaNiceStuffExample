a70122ee35fb57ef74bec3817ee7c605f6ce5e58
yaron.kessler <yaron.kessler41@google.com>
2017-09-16 10:10:42 +0300

    q&a

--- src/d_ClassAndObjects/G_MathClass.java
+++ src/d_ClassAndObjects/G_MathClass.java
@@ -3,12 +3,35 @@ package d_ClassAndObjects;
 public class G_MathClass {
 
 	public G_MathClass() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		double num1=33.3, num2=33.321;
+		double dblMax=0, dblMin=0;
 
-	}
+		
+		//return max value of parameters
+		dblMax = Math.max(num1,num2);
+		System.out.println(dblMax);
 
-}
+		
+		//return min value of parameters
+		dblMin = Math.min(num1,num2);
+		System.out.println(dblMin);		
+		
+		//TODO: MATH.* eample - more example/s.
+//		Math.abs() - return absolute value
+//		Math.ceil() - return round a floating point value up
+//		Math.floor() - return round a floating point value down
+//		Math.max() - return max value of parameters
+//		Math.min() - return min value of parameters
+//		Math.pow() - return power of second parameter
+//		Math.sqrt() - return square root value 
+//		Math.sin() - return sinus value
+//		Math.cos() - return cosine value
+		
+	} //main
+
+} //class
