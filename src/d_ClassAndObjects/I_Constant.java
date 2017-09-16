88b20d18a0bcd4cf3b620de0d2054402b8ba73a8
yaron.kessler <yaron.kessler41@google.com>
2017-09-17 01:49:17 +0300

    packages

--- src/d_ClassAndObjects/I_Constant.java
+++ src/d_ClassAndObjects/I_Constant.java
@@ -4,6 +4,11 @@ public class I_Constant
 {
 	private static final double PI = 3.14;
 	
+	//get pi value
+	public static double getPi() {
+		return PI;
+	}
+
 	public static void main(String[] args) 
 	{	
 		System.out.println("PI === " + PI);
---
+++ src/d_ClassAndObjects/J_ImportClasses.java
@@ -0,0 +1,12 @@
+package d_ClassAndObjects;
+
+import c_Array.A_ArrayBasic;
+
+public class J_ImportClasses 
+{
+	public static void main(String[] args) {
+		A_ArrayBasic a = new A_ArrayBasic();
+		a.ArrayOneDimensionalWrite();
+		a.ArrayOneDimensionalRead();
+	}
+}
