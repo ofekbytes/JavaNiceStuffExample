fb6464c3fb95b558bf98b0f1528033a802858c00
yaron <yaron.kessler41@gmail.com>
2017-09-28 12:11:37 +0300

    equal api

---
+++ src/e_ClassAdvanced/EnumsConstants/package-info.java
@@ -0,0 +1,9 @@
+/**** 
+ * -------------------------------------------------------------------------------------------
+ * .\e_ClassAdvanced.EnumsConstants\ 
+ * CardDecks.java 
+ * EnumsConstantsMain.java
+ * -------------------------------------------------------------------------------------------
+ */
+
+package e_ClassAdvanced.EnumsConstants;
--- src/e_ClassAdvanced/EqualMethod/Animal.java
+++ src/e_ClassAdvanced/EqualMethod/Animal.java
@@ -10,5 +10,59 @@ class Animal
 	Animal(int speed) {
 		intTestSpeed = speed;
 	}
+
+
+
+	
+	//---------------------------
+	
+	/* equals() method, generating automatically
+	 * "Source" --> "Generate hasCode() and equals()"
+	 *  
+	 *  when you implement "equals", you must implement "hashCode".
+	 *  
+	 * (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + intTestSpeed;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (intTestSpeed != other.intTestSpeed)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	/***
+	 * 
+	 * "source" --> "Generate getter and Setter" 
+	 */
+	
+	public int getIntTestSpeed() {
+		return intTestSpeed;
+	}
+
+
+	public void setIntTestSpeed(int intTestSpeed) {
+		this.intTestSpeed = intTestSpeed;
+	}
 	
 }//Animal Class
