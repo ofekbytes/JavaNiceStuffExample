86152b9297e141dcc18a0deff3f17d4c8ad736ed
kessler <yaron.kessler41@gmail.com>
2019-03-17 17:45:01 +0200

    java basic examples

---
+++ src/a_BasicEexample/JavaDocsExample.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package a_BasicEexample;
+
+/**
+ * @author yaron
+ *
+ */
+public class JavaDocsExample 
+{
+
+	public static void main(String [] args)
+	{
+		System.out.println("Java Docs Examples");
+		
+	}
+}
