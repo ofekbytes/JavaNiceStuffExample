ffd8bb2a2cfcee2de542fd308c6648fa9abf9fae
koskas <koskas@yahoo.com>
2020-03-10 19:25:56 +0200

    test

---
+++ src/h_databases/mySql/examples/ex01/Example1.java
@@ -0,0 +1,14 @@
+package h_databases.mySql.examples.ex01;
+
+public class Example1 {
+
+	public Example1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
